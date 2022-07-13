e5da9660f170ced1adb310a4464b1cd359a05cb7
Br-um <henriquedsdbrum@gmail.com>
2022-07-12 23:24:09 -0300

    Commit-1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import CardPessoa from './components/CardPessoa'
+function App() {
+  return (
+    <div className="App">
+      <h1>Bom dia</h1>
+      <CardPessoa nome="Henrique"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CardPessoa.jsx
@@ -0,0 +1,10 @@
+
+function CardPessoa(props){
+    return (
+        <div>
+            <h3>Bom dia, {props.nome}</h3>
+        </div>
+    )
+}
+
+export default CardPessoa
